5261d41dcad98a7afb34b69fb7e340d57d8ceb31
Guillaume Bort <guillaume.bort@gmail.com>
2013-11-04 15:54:01 +0100

    Update Dev Kit

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ require.config({
     jquery: 'vendor/jquery-2.0.3.min',
     underscore: 'vendor/underscore-1.5.2.min',
     backbone: 'vendor/backbone-1.0.0.min',
-    prismic: 'vendor/prismic.io-1.0.1.min',
+    prismic: 'vendor/prismic.io-1.0.3.min',
     text: 'vendor/text-0.27.0.min'
   },
 
